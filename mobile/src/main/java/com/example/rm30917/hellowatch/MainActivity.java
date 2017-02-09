dd095b3ea69cc95c9e968c7fe065926d073ced6d
Caue Bruno de Almeida <RM30917@fiap.com.br>
2017-02-08 22:27:15 -0200

    Initial commit

---
+++ mobile/src/main/java/com/example/rm30917/hellowatch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rm30917.hellowatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
